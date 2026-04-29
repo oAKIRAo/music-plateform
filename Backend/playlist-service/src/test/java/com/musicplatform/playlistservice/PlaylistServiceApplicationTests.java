92d7f62aae70eef2d5fdb89bbf54251d164ae19e
faisal <medfaicalmk@gmail.com>
2026-04-29 21:57:20 +0100

    added playlist folder who was missing

---
+++ Backend/playlist-service/src/test/java/com/musicplatform/playlistservice/PlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicplatform.playlistservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
